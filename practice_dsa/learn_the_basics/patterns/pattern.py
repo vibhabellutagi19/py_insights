a079bb5406bf90d81a0e2f9f2e81226682e6f29f
vibhabellutagi19 <vibhavari.bellutagi@gmail.com>
2024-06-11 18:01:02 +0200

    new patterns

--- practice_dsa/learn_the_basics/patterns/pattern.py
+++ practice_dsa/learn_the_basics/patterns/pattern.py
@@ -1,21 +1,35 @@
+from print_patterns import (
+    print_square,
+    print_triangle_stars,
+    print_triangle_numbers,
+    print_triangle_num_same,
+    print_triangle_ulta_star,
+    print_triangle_ulta_num,
+    print_triangle_equi_star,
+    print_triangle_equi_ulta
+)
 
-from print_patterns import print_square, print_triangle
+def print_pattern(n: int, fun: str):
+    patterns = {
+        "square": print_square,
+        "triangle": print_triangle_stars,
+        "triangle_num": print_triangle_numbers,
+        "triangle_num_same": print_triangle_num_same,
+        "tri_utla_star": print_triangle_ulta_star,
+        "tri_ulta_num": print_triangle_ulta_num,
+        "equi_star": print_triangle_equi_star,
+        "equi_ulta": print_triangle_equi_ulta
+    }
 
+    if fun in patterns:
+        patterns[fun](n)
+    else:
+        print(f"Invalid pattern function: {fun}")
 
-def print_pattern(n: int, fun: str ):
-    if fun == 'square':
-        print_square(n)
-    elif fun == 'triangle':
-        print_triangle(n)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     # read the input file
-    with open('practice_dsa/learn_the_basics/input.txt', 'r') as file:
+    with open("practice_dsa/learn_the_basics/input.txt", "r") as file:
         n = int(file.readline().strip())
-    
+
     # call the pattern function
-    #print_pattern(n, "square")
-    print_pattern(n, "triangle")
-
-
-    
+    print_pattern(n, "equi_ulta")
--- practice_dsa/learn_the_basics/patterns/print_patterns.py
+++ practice_dsa/learn_the_basics/patterns/print_patterns.py
@@ -2,11 +2,71 @@ def print_square(n: int):
     for i in range(0, n):
         for j in range(0, n):
             if j < n:
-                print("*", end = " ")
+                print("*", end=" ")
         print()
 
-def print_triangle(n: int):
+
+def print_triangle_stars(n: int):
     for i in range(0, n):
         for j in range(0, i + 1):
             print("*", end=" ")
+        print()
+
+
+def print_triangle_numbers(n: int):
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            print(j + 1, end=" ")
+        print()
+
+
+def print_triangle_num_same(n: int):
+    for i in range(0, n):
+        for j in range(0, i + 1):
+            print(i + 1, end=" ")
+        print()
+
+
+def print_triangle_ulta_star(n: int):
+    for i in range(n, 0, -1):
+        for j in range(0, i):
+            print("*", end=" ")
+        print()
+
+
+def print_triangle_ulta_num(n: int):
+    for i in range(n, 0, -1):
+        for j in range(0, i):
+            print(j + 1, end=" ")
+        print()
+
+
+def print_triangle_equi_star(n: int):
+    for i in range(0, n):
+        stars = i * 2 + 1
+        spaces = n - 1 - i
+
+        for st in range(0, spaces):
+            print(" ", end=" ")
+
+        for sp in range(0, stars):
+            print("*", end=" ")
+
+        for st in range(0, spaces):
+            print(" ", end=" ")
+        print()
+
+
+def print_triangle_equi_ulta(n: int):
+    for i in range(0, n):
+        stars = 2 * n - (i * 2 + 1)
+
+        for st in range(0, i):
+            print(" ", end=" ")
+
+        for sp in range(0, stars):
+            print("*", end=" ")
+
+        for st in range(0, i):
+            print(" ", end=" ")
         print()
